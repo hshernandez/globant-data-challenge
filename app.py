f32bee128cc449c6a00e3e3c8edb0ca864dfead0
hshernandez <139378328+hshernandez@users.noreply.github.com>
2023-07-13 17:04:47 -0500

    first working app

--- app.py
+++ app.py
@@ -1,28 +1,54 @@
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 import pyodbc
 import csv
 
 app = Flask(__name__)
+CORS(app)
 
 #Defualt route just to make sure API is up!!
 @app.route("/")
 def home():
+    print('HELLO WORLD')
     return "Hello, Flask!"
+
+def get_columns_tables(table_name):
+    with pyodbc.connect(conn_str) as conn:
+        cursor = conn.cursor()
+        columns_query = f"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
+        cursor.execute(columns_query)
+        row = cursor.fetchone()
+        columns = []
+        while row: 
+            columns.append(row)
+            row = cursor.fetchone()
+        conn.commit()
+    return ','.join([x[0] for x in columns])
+
+
 # Endpoint to receive CSV files
-
-@app.route('/upload', methods=['POST'])
-def upload_csv():
-    csv_files = request.files.getlist('file')  # assuming the files are uploaded as 'file' in the request form data
-
-    for csv_file in csv_files:
-        # Implement CSV parsing and data extraction logic here
-        data = parse_csv(csv_file)
-
-        # TODO: Validate the data
-
-        # TODO: Store the extracted data in memory or temporary storage
-
+@app.route('/upload/<table_name>', methods=['POST'])
+def upload_csv(table_name):
+    print(request.files)
+    if 'File' not in request.files:
+        return 'This method needs a csv file to operate. Please select a file', 400
+    
+    #validating file type
+    #content_type = request.headers.get('Content-Type')
+    #if (content_type != 'text/csv'):
+    #    return 'Not valid file type. It must be a csv', 400
+        
+    # assuming the files are uploaded as 'File' in the request form data
+    csv_files = request.files.getlist('File')
+    csv_file = csv_files[0].read().decode('ascii')
+    
+    data = csv_file.split('\n')
+    # TODO: Validate the data
+    for x in data:
+        print(x)
+        insert_data(table_name, x)
+   
     #Return message
     return 'CSV files uploaded successfully!'
 
@@ -34,6 +60,27 @@ def parse_csv(csv_file):
         # Process each row as per your requirements
         data.append(row)
     return data
+
+
+conn_str = (
+    "driver={ODBC Driver 17 for SQL Server};"
+    "SERVER=globant-challenge.database.windows.net;"
+    "DATABASE=globant-challenge;"
+    "UID=admin-login;"
+    "PWD=Hugosantiago98;"
+)
+
+def insert_data(table_name, data):
+    with pyodbc.connect(conn_str) as conn:
+        cursor = conn.cursor()
+        columns_query = get_columns_tables(table_name)
+        values = "'"+data.replace("'",' ').replace('\r','').replace(',',"','")+"'"
+        
+        insert_query = f"INSERT INTO {table_name} ({columns_query}) VALUES ({values})"
+        print(insert_query)
+        cursor.execute(insert_query)
+        conn.commit()
+
 
 
 
